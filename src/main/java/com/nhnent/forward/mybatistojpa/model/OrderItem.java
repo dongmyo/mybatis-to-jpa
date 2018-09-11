5adc7cbfd5351679b15a4a79f4dfffbfd378dcf5
신동민/NE <dongmyo@nhnent.com>
2018-09-11 16:38:59 +0900

    * model 에 json serialization을 위한 설정 추가

--- src/main/java/com/nhnent/forward/mybatistojpa/model/OrderItem.java
+++ src/main/java/com/nhnent/forward/mybatistojpa/model/OrderItem.java
@@ -1,14 +1,21 @@
 package com.nhnent.forward.mybatistojpa.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class OrderItem {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long orderId;
+
     private Integer limeNumber;
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long itemId;
+
     private Integer quantity;
 
 }
