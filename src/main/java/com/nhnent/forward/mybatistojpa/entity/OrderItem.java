41a4def91a6aa3501a9375c8d09694493db6309d
신동민/NE <dongmyo@nhnent.com>
2018-09-11 17:29:33 +0900

    * bug fix

--- src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderItem.java
+++ src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderItem.java
@@ -1,24 +1,26 @@
 package com.nhnent.forward.mybatistojpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Getter
 @Entity
 @Table(name = "OrderItems")
 public class OrderItem {
-    @Id
-    @Column(name = "order_id")
-    private Long orderId;
-
-    @Column(name = "line_number")
-    private Integer lineNumber;
+    @EmbeddedId
+    private Pk pk;
 
     @ManyToOne
     @JoinColumn(name = "item_id")
@@ -26,5 +28,25 @@ public class OrderItem {
 
     @Column
     private Integer quantity;
+
+
+    /*
+     * Composite-id class must implement serializable.
+     * Composite-id class must define equals and hashCode methods.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "order_id")
+        private Long orderId;
+
+        @Column(name = "line_number")
+        private Integer lineNumber;
+
+    }
 
 }
